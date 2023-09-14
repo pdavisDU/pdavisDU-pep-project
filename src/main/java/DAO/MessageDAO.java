42982cb550fb8756bb87acae36bca78991e56ec8
Paul Davis <pdavisDU@gmail.com>
2023-09-14 17:33:32 +0000

    setting up pseudo for messages

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,10 @@
 package DAO;
 
 public class MessageDAO {
-    
+    // create message
+    // delete messages
+    // get all messages
+    // get all messages for user
+    // get message by ID
+    // update message
 }
