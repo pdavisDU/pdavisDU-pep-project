e43a6fdc919307bd1e24c67360b5bb08a8dc998d
Paul Davis <pdavisDU@gmail.com>
2023-09-15 15:11:23 +0000

    Setting up get messages by ID with boilerplate from getMessages

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -66,6 +66,24 @@ public class MessageDAO {
     }
     // get messages by ID
     public Message getMessagesById(Int id) {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM message";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                int message_id = resultSet.getInt("message_id");
+                int posted_by = resultSet.getInt("posted_by");
+                String message_text = resultSet.getString("message_text");
+                long time_posted_epoch = resultSet.getInt("time_posted_epoch");
+                Message message = new Message(message_id, posted_by, message_text, time_posted_epoch);
 
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return messages;
     }
 }
